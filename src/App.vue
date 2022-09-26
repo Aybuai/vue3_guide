48f1191589239fd26a5bf66af41b42c31a0016db
Aybuai <13624448268@163.com>
2022-09-26 21:46:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/> -->
+  <div>
+    <h1>{{ count }}</h1>
+    <h1>{{ double }}</h1>
+    <h2>{{ helloStr }}</h2>
+    <h1>X:{{ pageX }}, Y:{{ pageY }}</h1>
+    <button @click="handleClick">👍🏻+1</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed, reactive, toRefs, watch } from "vue";
+import useMousePosition from "./hooks/useMousePosition";
+
+// const count = ref(0);
+
+// const double = computed(() => {
+//   return count.value * 2;
+// });
+
+// const handleClick = () => {
+//   count.value++;
+// };
+
+interface PublicType {
+  count: number;
+  double: number;
+  handleClick: () => void;
+}
+
+let helloStr = ref("");
+const data: PublicType = reactive({
+  count: 0,
+  double: computed(() => data.count * 2),
+  handleClick: () => {
+    data.count++;
+    helloStr.value += "hello! ";
+  },
+});
+
+// 监听
+watch([helloStr, () => data.count], (newVal, oldVal) => {
+  document.title = newVal[0];
+  console.log("oldVal", oldVal);
+  console.log("newVal", newVal);
+});
+
+// 需要转换一下响应式，不然解析出来后就失去了响应式
+const refData = toRefs(data);
+const { count, double, handleClick } = refData;
+
+// 导入的通用组件方法
+const { pageX, pageY } = useMousePosition();
+</script>
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/hooks/useMousePosition.ts
@@ -0,0 +1,29 @@
+import { ref, reactive, onMounted, onUnmounted, toRefs } from "vue";
+
+export default function useMousePosition() {
+  const x = reactive({
+    count: 0,
+  });
+  const y = reactive({
+    count: 0,
+  });
+
+  const mousePosition = (e: MouseEvent) => {
+    x.count = e.pageX;
+    y.count = e.pageY;
+  };
+
+  onMounted(() => {
+    document.addEventListener("click", mousePosition);
+  });
+
+  onUnmounted(() => {
+    document.removeEventListener("click", mousePosition);
+  });
+
+  // toRefs 防止 reactive 类型的数据失去响应式
+  const pageX = toRefs(x).count;
+  const pageY = toRefs(y).count;
+
+  return { pageX, pageY };
+}
